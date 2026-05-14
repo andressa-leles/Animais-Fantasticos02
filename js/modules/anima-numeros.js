90f1b7df8c0b1b21802cee74eb64059ef26a1416
Andressa Leles <leles.andressa@gmail.com>
2026-05-14 17:53:11 -0300

    executando primeiro build com webpack

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -29,3 +29,4 @@ export default function initAnimaNumeros() {
 
   observer.observe(observeTarget, { attributes: true });
 }
+
--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -3,9 +3,11 @@ export default function initFetchBitcoin() {
     .then((response) => response.json())
     .then((bitcoin) => {
       const btcPreco = document.querySelector(".btc-preco");
+      // bitcoin é o nome correto do parâmetro que você definiu acima
       btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
-      console.log(json);
-    }).catch(erro =>{
-      console.log(Error(erro))
+      console.log(bitcoin);
     })
+    .catch((erro) => {
+      console.log(Error(erro));
+    });
 }
